a977ba21b1c8f3ec1981e1910107db7874af26a1
kiwi-jang <kiwi.jang@toss.im>
2022-10-29 14:22:54 +0900

    style: Change tsconfig path to tsconfig.json

--- packages/eslint-config/src/eslint.js
+++ packages/eslint-config/src/eslint.js
@@ -1,24 +1,12 @@
 module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: ['plugin:@typescript-eslint/recommended'],
-	plugins: [
-		'@typescript-eslint/eslint-plugin',
-		'simple-import-sort',
-		'unused-imports',
-	],
-	parserOptions: {
-		project: 'tsconfig.eslint.json',
-		sourceType: 'module',
-	},
+	plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports'],
 	rules: {
-		'simple-import-sort/imports': 'error',
-		'simple-import-sort/exports': 'error',
-
-		'unused-imports/no-unused-imports-ts': 'error',
-
-		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
+
+		'unused-imports/no-unused-imports-ts': 'error',
 	},
 };
